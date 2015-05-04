f6838ba56a875189729f6339eb2cc61e4ee45868
Garrett Graham <ganderson7197@icloud.com>
2015-05-04 11:51:47 -0400

    getting the email set up through mandrill but may move onto fireform just to remind myself how this all works

--- js/contact_me.js
+++ js/contact_me.js
@@ -21,13 +21,31 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
+                url: "https://mandrillapp.com/api/1.0/messages/send-template.json",
+                method: "POST",
                 data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
+                    'key': 'Be50ItuT9NzIlPX2ESdEUQ',
+					'template_name': 'Portfolio',
+					'template_content': [
+						{
+							'name':'example name',
+							'content': 'example content'
+						}
+					  ],
+					'message': {
+					  'from_email': email,
+						'from_name': name,
+						'headers': {
+							'Reply-To': email
+						},
+						'subject': 'Job Lead',
+						'to': [
+							{
+								'email': 'ganderson7197@icloud.com',
+								'name': 'Garrett Anderson',
+								'type': 'to'
+							}],
+					}
                 },
                 cache: false,
                 success: function() {
